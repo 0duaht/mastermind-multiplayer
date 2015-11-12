2c8e95166640b97bf98cb93234d3c9f56a526423
Tobi Oduah <tobi.oduah@andela.com>
2015-11-12 10:02:28 +0100

    bug fix

--- lib/single_game.rb
+++ lib/single_game.rb
@@ -61,4 +61,83 @@ class SinglePlayer
     wrong_guess(sequence, guesses, input, history)
     return guesses
   end
+  
+  def print_top_ten
+    top_ten_list = YAML.load_stream(File.open(UI::DB_STORE)).sort{|player1, player2|  # load player objects from db and sort by guesses/time
+      by_guess = player1.guesses <=> player2.guesses
+      by_guess == 0 ? player1.time <=> player2.time : by_guess
+    }[0...10]  if File.file?(UI::DB_STORE) 
+    
+    puts UI::TOP_TEN
+    top_ten_list.each_with_index{|player, index| puts "#{index+1}. " + player.to_s }
+  end
+  
+  def right_guess(start_time, sequence, guesses)
+    time_elapsed = (Time.now - start_time).to_i
+    name = store_game(sequence, guesses, time_elapsed)
+    
+    puts UI::CONGRATS_MESSAGE % [name, sequence.join.upcase, guesses, guesses > 1 ? "guesses" : "guess", time_convert(time_elapsed)]
+    print_top_ten
+    print UI::OPTIONS_MESSAGE + UI::INPUT_PROMPT
+    user_choice
+  end
+  
+  def wrong_guess(sequence, guesses, input, history)
+    result = GameLogic.check_input(sequence, input)                                       # get results from input
+    history << GamePlay.new(input, result[:correct_elements], result[:correct_position])  # add game play to history
+    
+    puts UI::INFO_MESSAGE % [input.upcase, result[:correct_elements], result[:correct_position]]
+    print UI::GUESSES_MESSAGE % [guesses, guesses > 1 ? "guesses" : "guess"]
+  end
+  
+  def store_game(sequence, guesses, time)      #get player name and store details to file  
+    print UI::NAME_MESSAGE
+    name = get_name
+    current_player = Player.new(name, sequence, time, guesses)  # create new player object
+    
+    # write player object to file if file does not exist, or verify whether to add record from user, and write if it exists
+    File.open(UI::DB_STORE, 'a'){|file| file.write(YAML.dump(current_player))} if File.exist?(UI::DB_STORE) && user_permits_store
+     
+    name
+  end
+  
+  def user_permits_store
+    print UI::OVERWRITE_MESSAGE
+    print UI::INPUT_PROMPT
+    option_chosen = false
+    
+    while !option_chosen
+      option_chosen = true                              # assume user selects valid option so as to quit loop
+      
+      input = gets.chomp.downcase
+      case input                                        
+      when "y", "yes" then return true
+      when "n", "no" then return false
+      else                                               # user selects an invalid option
+        print UI::INVALID_MESSAGE
+        option_chosen = false
+      end  
+    end
+  end
+  
+  def get_name
+    name = ""
+    
+    while name.eql?("")
+      name = gets.chomp.capitalize
+      print UI::INVALID_MESSAGE if name.eql?("")  
+    end
+    
+    name
+  end
+   
+  def print_history(history)
+    if history.empty?
+      print "No history yet. Enter a guess"  + UI::INPUT_PROMPT
+    else
+      puts ""
+      puts history
+      print UI::INPUT_PROMPT 
+    end
+  end
 end
