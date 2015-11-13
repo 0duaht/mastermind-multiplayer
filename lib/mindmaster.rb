6cb45ea3147ee33291168ae710c9063cae4d6350
Tobi Oduah <tobi.oduah@andela.com>
2015-11-13 17:07:59 +0100

    renamed files

---
+++ lib/mindmaster.rb
@@ -0,0 +1,14 @@
+require 'mastermind/ui'
+require 'mastermind/gamehelper'
+
+module MasterMind
+  VERSION = 1.0
+  extend GameHelper
+  
+  def self.start
+    puts UI::WELCOME_MESSAGE
+    print UI::OPTIONS_MESSAGE + UI::INPUT_PROMPT
+    
+    user_choice
+  end
+end
