7b58afa742bf534d00b7fe334036a1975e3f01c4
Tobi Oduah <tobi.oduah@andela.com>
2015-11-13 17:25:55 +0100

    fixed issues with moving file

---
+++ lib/mastermind/single_game.rb
@@ -0,0 +1,176 @@
+require 'ui'
+require 'timehelper'
+require 'player'
+require 'yaml'
+require 'gameplay'
+
+class SinglePlayer
+  
+  include TimeHelper
+  
+  attr_reader :start_time
+  attr_reader :history
+  attr_reader :sequence
+  attr_reader :game_logic
+  attr_accessor :end_guess
+  
+  ALLOWED = ['c', 'h', 'q', 'cheat', 'history', 'quit']
+  
+  def initialize(sequence, game_logic)
+    @start_time = Time.now
+    @history = []
+    @sequence = sequence
+    @game_logic = game_logic
+    @end_guess = 13
+  end
+  
+  # generate game sequence and start game play
+  def start_game
+    print UI::GENERATE_MESSAGE % [game_logic.sequence_type, game_logic.length, UI::COLOR_STRINGS[game_logic.level]]
+    print UI::INPUT_PROMPT
+    guesses = 0
+    
+    # allow the user guess up to twelve times before ending game
+    while guesses < UI::GUESS_MAX
+      input = gets.chomp.downcase
+      next if invalid_length?(input)
+      next if treat_option?(input, history)
+      guesses = treat_guess(input, guesses, history)
+    end
+    puts UI::SORRY_SINGLE_MESSAGE % sequence.join.upcase if guesses == UI::GUESS_MAX   
+  end
+  
+  # check if user's guess is longer or fewer than the required length
+  def invalid_length?(input)   
+    if input.length < game_logic.length && !(ALLOWED.include?(input))
+      print UI::INPUT_SHORT_MESSAGE
+      return true
+      
+    elsif input.length > game_logic.length && !(ALLOWED.include?(input)) 
+      print UI::INPUT_LONG_MESSAGE
+      return true
+    end
+    
+    return false
+  end
+  
+  # check if user selects an option
+  def treat_option?(input, history)
+    case input
+    when "h", "history" then print_history(history)
+    when "q", "quit" then exit(0)
+    when "c", "cheat" then print UI::SEQUENCE_MESSAGE % sequence.join.upcase
+    else return false
+    end
+    return true
+  end
+  
+  # treat guesses entered by user
+  def treat_guess(input, guesses, history)
+    guesses += 1
+    if input == sequence.join                         # right guess entered
+      right_guess(start_time, sequence, guesses)
+      guesses = end_guess                             # sentinel value to end guess loop
+    else
+      wrong_guess(sequence, guesses, input, history)  # wrong guess entered
+    end
+    
+    return guesses
+  end
+  
+  def print_top_ten(current_player)
+    top_ten_list = YAML.load_stream(File.open(UI::DB_STORE)).sort{|player1, player2|  # load player objects from db and sort by guesses/time
+      by_guess = player1.guesses <=> player2.guesses                                  # first sort by guesses
+      by_guess == 0 ? player1.time <=> player2.time : by_guess                        # then sort by time
+    }[0...10]  if File.file?(UI::DB_STORE)                                            # pick out top ten
+    
+    puts average_string(top_ten_list, current_player) if top_ten_list.length > 1     # print out user's performance compared to average
+    
+    # print out top ten results
+    if !top_ten_list.nil?
+      puts ""
+      puts UI::TOP_TEN                                                                  
+      top_ten_list.each_with_index{|player, index| puts "#{index+1}. " + player.to_s }
+    end
+  end
+  
+  def right_guess(start_time, sequence, guesses)
+    time_elapsed = (Time.now - start_time).to_i                                       # time used by user in seconds
+    current_player = store_game(sequence, guesses, time_elapsed)                      # store user data to top-scores file
+    
+    puts UI::CONGRATS_MESSAGE % [current_player.name, sequence.join.upcase, guesses, guesses > 1 ? "guesses" : "guess", 
+      time_convert(time_elapsed) << '.']
+    print_top_ten(current_player)
+  end
+  
+  def average_string(top_ten_list, current_player)                                    # generates user's performance compared to average
+    # time difference obtained
+    time_diff = (top_ten_list.inject(0){ |sum, player| sum += player.time } / top_ten_list.length.to_f).round - current_player.time
+    # guess difference obtained
+    guess_diff = (top_ten_list.inject(0){ |sum, player| sum += player.guesses } / top_ten_list.length.to_f).round - current_player.guesses
+    
+    "That's %s %s and %s %s %s the average\n" % [time_convert(time_diff.abs), time_diff < 0 ? "slower" : "faster",
+      guess_diff.abs, guess_diff.abs == 1 ? "guess" : "guesses", guess_diff < 0 ? "more" : "fewer"]
+  end
+  
+  def wrong_guess(sequence, guesses, input, history)
+    result = GameLogic.check_input(sequence, input)                                       # get results from input
+    history << GamePlay.new(input, result[:correct_elements], result[:correct_position])  # add game play to history
+    
+    puts UI::INFO_MESSAGE % [input.upcase, result[:correct_elements], result[:correct_position]]
+    puts UI::GUESSES_MESSAGE % [guesses, guesses > 1 ? "guesses" : "guess"]
+    print UI::INPUT_PROMPT
+  end
+  
+  def store_game(sequence, guesses, time)      #get player name and store details to file  
+    print UI::NAME_MESSAGE
+    name = get_name
+    current_player = Player.new(name, sequence, time, guesses)  # create new player object
+    
+    # write player object to file if file does not exist, or verify whether to add record from user, and write if it exists
+    File.open(UI::DB_STORE, 'a'){|file| file.write(YAML.dump(current_player))} if user_permits_store?
+     
+    current_player
+  end
+  
+  def user_permits_store?                    # confirm from user to add record to top-scores if file exists
+    return true if !File.exist?(UI::DB_STORE)    # if file does not exist, return true
+    print UI::OVERWRITE_MESSAGE
+    print UI::INPUT_PROMPT
+    option_chosen = false
+    
+    while !option_chosen
+      option_chosen = true                              # assume user selects valid option so as to quit loop
+      
+      input = gets.chomp.downcase
+      case input                                        
+      when "y", "yes" then return true
+      when "n", "no" then return false
+      else                                               # user selects an invalid option
+        print UI::INVALID_MESSAGE
+        option_chosen = false
+      end  
+    end
+  end
+  
+  def get_name
+    name = ""
+    
+    while name.eql?("")
+      name = gets.chomp.capitalize
+      print UI::INVALID_MESSAGE if name.eql?("")  
+    end
+    
+    name
+  end
+   
+  def print_history(history)
+    if history.empty?
+      print "No history yet. Enter a guess"  + UI::INPUT_PROMPT
+    else
+      puts ""
+      puts history
+      print UI::INPUT_PROMPT 
+    end
+  end
+end
---
+++ lib/mastermind/ui.rb
@@ -0,0 +1,67 @@
+module UI
+  
+  GUESS_MAX = 12
+  
+  WELCOME_MESSAGE = "\nWelcome to the Classy MasterMind game by Tobi\n"
+  
+  INPUT_PROMPT = "\n>>  "
+  
+  OPTIONS_MESSAGE = "\nWould you like to (p)lay, (r)ead the instructions or (quit) ?"
+    
+  HELP_MESSAGE = "\nOnce game starts, a random code is generated and you're asked to guess what the code is.
+Every code is a four/five/six digit word made up by the letters [r, g, b, y]/[r, g, b, y, o]/[r, g, b, y, o, v] 
+depending on difficulty levels beginner/intermediate/advanced levels. You have twelve guesses per game.
+On every guess, you are presented with a message identifying the number of elements you got correctly, and in what positions.
+To view entry history, enter h or history at any time
+To view sequence generated, enter c or cheat at any time
+To quit the game at any point enter q or quit\n\n"
+  
+  BEGINNER_COLOR = "(r)ed, (g)reen, (b)lue and (y)ellow"
+  
+  INTERMEDIATE_COLOR = "(r)ed, (g)reen, (b)lue, (y)ellow, and (o)range"
+  
+  ADVANCED_COLOR = "(r)ed, (g)reen, (b)lue, (y)ellow, (o)range and (v)iolet"
+ 
+  COLOR_STRINGS = [BEGINNER_COLOR, INTERMEDIATE_COLOR, ADVANCED_COLOR]
+  
+  GENERATE_MESSAGE = "\nI have generated %s sequence with %s elements made \
+up of: %s\nUse (q)uit at any time to end the game. \nWhat's your guess?"
+  
+  INVALID_MESSAGE = "\nEntry invalid! Please try again" + INPUT_PROMPT
+  
+  INPUT_SHORT_MESSAGE = "Input too short. Please enter guess correctly" + INPUT_PROMPT
+  
+  INPUT_LONG_MESSAGE = "Input too long. Please enter guess correctly." + INPUT_PROMPT
+  
+  SEQUENCE_MESSAGE = "The sequence is %s" + INPUT_PROMPT
+  
+  CONGRATS_MESSAGE = "\n%s, You guessed the sequence '%s' in %s %s over %s"
+  
+  INFO_MESSAGE = "'%s' has %s of the correct elements with %s in the correct positions."
+  
+  END_MESSAGE = "Do you want to (p)lay again or (q)uit?"
+  
+  GUESSES_MESSAGE = "You've taken %s %s"
+  
+  LEVEL_MESSAGE = "\nDifficulty level: (b)eginner, (i)ntermediate or (a)dvanced? " + INPUT_PROMPT
+  
+  NAME_MESSAGE = "\nCongratulations! You've guessed the sequence! What's your name?" + INPUT_PROMPT
+  
+  DB_STORE = 'store.yaml'
+  
+  TOP_TEN = '=== TOP TEN ==='
+  
+  OVERWRITE_MESSAGE = 'Data file already exists. Do you want to add your score to top scores? (y)es or (n)o..'
+  
+  MODE_SELECT = "\nPlease choose mode >>> (s)ingle or (m)ulti player mode?" + INPUT_PROMPT
+  
+  MULTI_START_MESSAGE = "Welcome to the MultiPlayer Challenge. How many users will be playing today?" + INPUT_PROMPT
+  
+  PLAYER_MESSAGE = "********Player %s's turn" + INPUT_PROMPT
+  
+  SORRY_SINGLE_MESSAGE = "Sorry, You lost. The sequence is %s"
+  
+  SORRY_MULTI_MESSAGE = "Sorry, You all lost. The sequence is %s"
+  
+  PASSWORD_MESSAGE = "\nWould you like to hide each player's guesses? (y)es or (n)o ?" + INPUT_PROMPT 
+end
