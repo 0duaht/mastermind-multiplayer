e2e3028850413ae2c6757934b61363d6538d6e1a
Tobi Oduah <tobi.oduah@andela.com>
2015-11-14 22:00:12 +0100

    refactor timehelper

--- lib/mastermind/tobi/timehelper.rb
+++ lib/mastermind/tobi/timehelper.rb
@@ -1,6 +1,8 @@
 module MasterMind
   module Tobi
     module TimeHelper
+      SINGULAR = [' hour', ' minute', ' second']
+      PLURAL = [' hours', ' minutes', ' seconds']
       def time_convert(time_int)
         hours = time_int / 3600
         minutes = (time_int % 3600) / 60
@@ -10,24 +12,16 @@ module MasterMind
       
       def construct_string(hours, minutes, seconds)
         time_string = ''
-        time_string = hour_helper(hours, minutes, time_string)
-        time_string = minute_helper(minutes, seconds, time_string)
+        time_string = hour_minute_helper(hours, minutes, time_string, 0)
+        time_string = hour_minute_helper(minutes, seconds, time_string, 1)
         time_string = second_helper(seconds, time_string)
         time_string
       end
       
-      def hour_helper(hours, minutes, time_string)
-        if hours >= 1
-          time_string << (hours.to_s + (hours == 1 ? ' hour' :' hours'))
-          time_string << ', ' if minutes > 0
-        end
-        time_string
-      end
-      
-      def minute_helper(minutes, seconds, time_string)
-        if minutes >= 1
-          time_string << (minutes.to_s + (minutes == 1 ? ' minute' : ' minutes'))
-          time_string << ', ' if seconds > 0
+      def hour_minute_helper(greater, lesser, time_string, position)
+        if greater >= 1
+          time_string << (greater.to_s + (greater == 1 ? SINGULAR[position] : PLURAL[position]))
+          time_string << ', ' if lesser > 0
         end
         time_string
       end
