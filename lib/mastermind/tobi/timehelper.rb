050b668ae35cedb490cb95fea369f7b4e31d4b23
Tobi Oduah <tobi.oduah@andela.com>
2015-11-14 21:52:14 +0100

    refactor timehelper

--- lib/mastermind/tobi/timehelper.rb
+++ lib/mastermind/tobi/timehelper.rb
@@ -9,23 +9,35 @@ module MasterMind
       end
       
       def construct_string(hours, minutes, seconds)
-        time_string = '' 
+        time_string = ''
+        time_string = hour_helper(hours, minutes, time_string)
+        time_string = minute_helper(minutes, seconds, time_string)
+        time_string = second_helper(seconds, time_string)
+        time_string
+      end
+      
+      def hour_helper(hours, minutes, time_string)
         if hours >= 1
           time_string << (hours.to_s + (hours == 1 ? ' hour' :' hours'))
           time_string << ', ' if minutes > 0
-        end    
-         
+        end
+        time_string
+      end
+      
+      def minute_helper(minutes, seconds, time_string)
         if minutes >= 1
           time_string << (minutes.to_s + (minutes == 1 ? ' minute' : ' minutes'))
           time_string << ', ' if seconds > 0
         end
-         
+        time_string
+      end
+      
+      def second_helper(seconds, time_string)
         if seconds >= 1
           time_string << (seconds.to_s + (seconds == 1 ? ' second' : ' seconds'))
         elsif seconds == 0 && time_string == ''
           time_string << '0 seconds'
         end
-        
         time_string
       end
     end
