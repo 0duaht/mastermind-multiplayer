20546dd0dfd8b687bacbf7cff839f2f755b583ac
Tobi Oduah <tobi.oduah@andela.com>
2015-11-15 10:09:03 +0100

    game helper tests

--- spec/gamehelper_spec.rb
+++ spec/gamehelper_spec.rb
@@ -7,6 +7,7 @@ describe "GameHelper" do
   let(:beginner) {0}
   let(:intermediate) {1}
   let(:advanced) {2}
+  let(:num_of_players) {2}
   
   subject do
     MasterMind::Tobi::GameHelper.new
@@ -29,7 +30,7 @@ describe "GameHelper" do
       expect(subject.ask_level).to eql(advanced) 
     end
     
-    it "allows user select other options" do
+    it "allows user select to play and play single-player smoothly" do
       Object.any_instance.stub(:gets) {
         @counter ||= 0
         response = ''
@@ -49,6 +50,34 @@ describe "GameHelper" do
         elsif @counter >= 6 && @counter < 19
           response = 'JJJJ'
         elsif @counter >= 19
+          response = 'q'
+        end
+        @counter += 1
+        response
+      }
+      expect{subject.user_choice}.to raise_error SystemExit
+    end
+    
+    it "allows user select to play and play multi-player smoothly" do
+      Object.any_instance.stub(:gets) {
+        @counter ||= 0
+        response = ''
+        
+        if @counter == 0
+          response = 'p'
+        elsif @counter == 1
+          response = 'g'   
+        elsif @counter == 2
+          response = 'b'
+        elsif @counter == 3
+          response = 'm'
+        elsif @counter == 4
+          response = 'no'
+        elsif @counter == 5
+          response = num_of_players.to_s
+        elsif @counter > 5 && @counter < 30
+          response = 'JJJJJ'
+        else
           response = 'q'
         end
         @counter += 1
